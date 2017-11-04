551764c2b7f6b982c46f414a61c0ebe719fb0f47
Chris Harvey <chharvey@icloud.com>
2017-11-04 00:04:21 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,11 +9,18 @@ class Book
   end
 
   def author= (author)
-    @author= author
+    @author = author
   end
 
   def author
     @author
+  end
+
+  def page_count= (num)
+    @num = num
+  end
+
+  def page_count
   end
 
 end
