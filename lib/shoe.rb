c090224f60534f2129faee30b84bde3eaa0812e1
Chris Harvey <chharvey@icloud.com>
2017-11-04 00:17:31 +0000

    automatically backed up by learn

---
+++ lib/shoe.rb
@@ -0,0 +1,14 @@
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize (brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "You shoe is as good as new"
+  end
+
+end
